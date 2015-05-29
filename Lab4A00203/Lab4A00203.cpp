df11f2c92a9ed9b226e8a00fb839af0ade157c3d
dianaalse <dianaalse@gmail.com>
2015-05-29 17:47:07 -0600

    primer codigo

---
+++ Lab4A00203/Lab4A00203.cpp
@@ -0,0 +1,13 @@
+// Lab4A00203.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hola GitHub" << endl;
+	return 0;
+}
+
